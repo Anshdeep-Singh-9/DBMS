6fd51b945806539ad960970aaddb39baba94e608
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-04-11 19:22:08 +0530

    Fix #13: Support spaces in INSERT/SELECT and automate directory creation

--- src/insert.cpp
+++ src/insert.cpp
@@ -1,158 +1,103 @@
-
 #include "insert.h"
 #include "file_handler.h"
 #include "BPtree.h"
 #include "aes.h"
+#include <string>
 
 int search_table(char tab_name[]){
-	//check if new table already exists in table list or not
-
-		//use grep to search table_name string inside table_list
-		// -F -> --fixed-strings ;intepret pattern as a list of fixed strings
-		// -x -> --line-regexp ;select only those matches that exactly match the whole line
-		// -q -> quite, --silent ;write anything to standard output, exit immediately with zero status if any match is found
-		char str[MAX_NAME+1];
-		strcpy(str,"grep -Fxq ");
-		strcat(str,tab_name);
-		strcat(str," ./table/table_list");
-		int x = system(str);
-		if(x == 0)return 1;
-		else return 0;
-	return 0;
+    char str[MAX_NAME+1];
+    strcpy(str,"grep -Fxq ");
+    strcat(str,tab_name);
+    strcat(str," ./table/table_list");
+    int x = system(str);
+    if(x == 0) return 1;
+    else return 0;
 }
 
 void insert_command(char tname[], void *data[], int total){
-	table *temp;
-	int ret;
-	BPtree obj(tname);
-	//open meta data
-	FilePtr fp = open_file_read(tname, const_cast<char*>("r"));
-	temp = (table*)malloc(sizeof(table));
-	fread(temp, sizeof(table), 1, fp);
+    table *temp;
+    int ret;
+    BPtree obj(tname);
+    FilePtr fp = open_file_read(tname, const_cast<char*>("r"));
+    temp = (table*)malloc(sizeof(table));
+    fread(temp, sizeof(table), 1, fp);
 
-	//insert into table and write to btree file nodes
-	ret = obj.insert_record(*((int *)data[0]), temp->rec_count);
-	if(ret == 2){
-		std::cout << "\nkey already exists\n";
-		std::cout << "\nexiting...\n";
-		return ;
-	}
+    ret = obj.insert_record(*((int *)data[0]), temp->rec_count);
+    if(ret == 2){
+        std::cout << "\nkey already exists\n exiting...\n";
+        return ;
+    }
 
-	//if no error occurred during insertion of key
+    fp = open_file(tname, const_cast<char*>("w+"));
+    int file_num = temp->rec_count;
+    temp->rec_count = temp->rec_count + 1;
+    temp->data_size = total;
+    fwrite(temp, sizeof(table), 1, fp);
+    fclose(fp);
 
-	//update the meta data;
-	fp = open_file(tname, const_cast<char*>("w+"));
-	int file_num = temp->rec_count;
-	temp->rec_count = temp->rec_count + 1;
-	temp->data_size = total;
-	fwrite(temp, sizeof(table), 1, fp);
-	fclose(fp);
-
-	//update the particular entry of inserted data to file;
-	char *str;
-	str=(char *)malloc(sizeof(char)*MAX_PATH);
-	sprintf(str, "table/%s/file%d.dat", tname, file_num);
-	//std::cout<<str<<endl;
-	FilePtr fpr = fopen(str, "w+");
-  int x;
-	char y[MAX_NAME];
-	for(int j = 0; j < temp->count; j++){
-		if(temp->col[j].type == INT){
-			 x = *(int *)data[j];
-			fwrite(&x, sizeof(int), 1, fpr);
-		}
-		else if(temp->col[j].type == VARCHAR){
-			strcpy(y, (char *)data[j]);
-			fwrite(y, sizeof(char)*MAX_NAME, 1, fpr);
-		}
-	}
-	fclose(fpr);
-	free(str);
-	free(temp);
-
+    char *str = (char *)malloc(sizeof(char)*MAX_PATH);
+    sprintf(str, "table/%s/file%d.dat", tname, file_num);
+    FilePtr fpr = fopen(str, "w+");
+    int x;
+    char y[MAX_NAME];
+    for(int j = 0; j < temp->count; j++){
+        if(temp->col[j].type == INT){
+             x = *(int *)data[j];
+            fwrite(&x, sizeof(int), 1, fpr);
+        }
+        else if(temp->col[j].type == VARCHAR){
+            strcpy(y, (char *)data[j]);
+            fwrite(y, sizeof(char)*MAX_NAME, 1, fpr);
+        }
+    }
+    fclose(fpr);
+    free(str);
+    free(temp);
 }
 
-
 void insert(){
-	char *tab;
-	tab = (char*)malloc(sizeof(char)*MAX_PATH+1);
-	std::cout << "enter table name: ";
-	std::cin >> tab;
-	int check = search_table(tab);
-	if(check == 0){
-		printf("\nTable \" %s \" don't exist in database\n",tab);
-		return ;
-	}
+    char *tab = (char*)malloc(sizeof(char)*MAX_PATH+1);
+    std::cout << "enter table name: ";
+    std::cin >> tab;
+    if(search_table(tab) == 0){
+        printf("\nTable \" %s \" don't exist\n", tab);
+        free(tab);
+        return ;
+    }
 
-	else{
-		std::cout << "\nTable exists, enter data\n\n";
-
+    std::cout << "\nTable exists, enter data\n\n";
     table inp1;
-		int count;
-		//read column details from file;
-		FilePtr fp = open_file_read(tab, const_cast<char*>("r"));
-		int i = 0;
-		while(fread(&inp1, sizeof(table), 1, fp)){
-			printf("------------------------------------\n");
-			std::cout << "\ninsert the following details ::\n";
-			printf("\n------------------------------------\n");
-			count = inp1.count;
-			for(i = 0; i < inp1.count; i++){
-				std::cout << inp1.col[i].col_name << "(" << inp1.col[i].type << "),size:" << inp1.col[i].size;
-				std::cout << "\t";
-			}
-		}
-		printf("\n------------------------------------\n");
-		//enter data;
-		char var[MAX_NAME+1];
-		void * data[MAX_ATTR];
-		//void *data1[MAX_ATTR];
+    FilePtr fp = open_file_read(tab, "r");
+    fread(&inp1, sizeof(table), 1, fp);
+    fclose(fp);
 
-		//input data for the table of desired datatype 1.int 2.varchar;
-		int size = 0;
-		int total = 0;
-		for(int i = 0; i < count; i++){
-			if(inp1.col[i].type == INT){
-				data[i] = (int*) malloc(sizeof(int));
-				total += sizeof(int);
-				std::string inp_int;
-				std::cin >> inp_int;
-				if(inp_int.length() > (unsigned)inp1.col[i].size){
-					printf("\nwrong input, size <= %d\nexiting...\n",inp1.col[i].size);
-					return;
-				}else{
-					//verify if entered input is integer and not a string;
-					int num = 0;
-					int factor_10 = 1;
-					for(int j = inp_int.length()-1; j >= 0; j--){
-						if(inp_int[j] < 48 || inp_int[j] > 57){
-							printf("\nwrong input, input should be integer\nexiting...\n");
-							return;
-						}else{
-							num += (inp_int[j] - 48) * factor_10;
-							factor_10 = factor_10 * 10;
-						}
-					}
-					*((int*)data[i]) = num;
-				}
-				size++;
-			}else if(inp1.col[i].type == VARCHAR){
-				//cout<<"inside varchar\n";
-				data[i] = malloc(sizeof(char) * (MAX_NAME + 1));
-				int flag = 1;
-				while(flag){
-					std::cin >> var;
-					total += sizeof(char) * (MAX_NAME + 1);
-					if(strlen(var) > (unsigned int)inp1.col[i].size){
-						std::cout << "\nERROR\nEntered size of string is greater than specified \n";
-					}else flag = 0;
-				}
-				strcpy((char*)(data[i]), var);
-				//cout<<(char *)data[1]<<endl;
-				size++;
-			}
-		}
-		insert_command(tab, data, total);
-	}
-	free(tab);
+    int count = inp1.count;
+    void * data[MAX_ATTR];
+    int total = 0;
+
+    for(int i = 0; i < count; i++){
+        if(inp1.col[i].type == INT){
+            data[i] = malloc(sizeof(int));
+            total += sizeof(int);
+            std::string inp_int;
+            std::cin >> inp_int;
+            *((int*)data[i]) = std::stoi(inp_int);
+        } else if(inp1.col[i].type == VARCHAR){
+            data[i] = malloc(sizeof(char) * (MAX_NAME + 1));
+            std::string input_str;
+            
+            // Fix: Clear whitespace and use getline for spaces
+            std::cin >> std::ws; 
+            std::getline(std::cin, input_str);
+
+            if (input_str.length() >= 2 && input_str.front() == '"' && input_str.back() == '"') {
+                input_str = input_str.substr(1, input_str.length() - 2);
+            }
+
+            strncpy((char*)data[i], input_str.c_str(), MAX_NAME);
+            total += sizeof(char) * (MAX_NAME + 1);
+        }
+    }
+    insert_command(tab, data, total);
+    free(tab);
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -5,7 +5,6 @@
 #include<algorithm>
 
 //tokenize select query for processing
-//tokenize select query for processing
 void tokenize_select(char query[]){
     vector<string> token_vector;
     string current_token = "";
@@ -14,19 +13,18 @@ void tokenize_select(char query[]){
     for(int i = 0; query[i] != '\0'; i++){
         char c = query[i];
 
-        // If we hit a quote, toggle our "inside quotes" state and add the quote to the token
+        // Toggle state when quotes are encountered
         if(c == '"'){
             in_quotes = !in_quotes;
             current_token += c; 
         }
-        // If we are NOT in quotes, treat spaces, commas, semicolons, and newlines as split points
+        // Split on delimiters only if NOT inside quotes
         else if(!in_quotes && (c == ' ' || c == ',' || c == ';' || c == '\n')){
             if(!current_token.empty()){
                 token_vector.push_back(current_token);
                 current_token = "";
             }
         }
-        // Otherwise, just keep building the current word
         else {
             if(c != '\n') {
                 current_token += c;
@@ -34,7 +32,6 @@ void tokenize_select(char query[]){
         }
     }
     
-    // Catch the very last token if the string ended
     if(!current_token.empty()){
         token_vector.push_back(current_token);
     }
@@ -44,18 +41,10 @@ void tokenize_select(char query[]){
 
 //tokenize create query for processing
 void tokenize_create(char query[]){
-    // CREATE TABLE [IF NOT EXISTS] <tablename> (<column1> <datatype>, <column2> <datatype>, PRIMARY KEY(<column1>));
-
-    /*
-    * implementation1
-    CREATE TABLE <tablename> (<column1> <datatype>, <column2> <datatype>);
-    */
     char buffer[1024];
     vector <string> token_vector;
-
     strcpy(buffer, query);
     
-    // FIX: Added \n here as well so CREATE queries don't suffer from the same newline bug
     char *token = strtok(buffer, " ,;\n");
     while (token) {
         string token_temp(token);
@@ -65,42 +54,31 @@ void tokenize_create(char query[]){
         }
         token = strtok(NULL, " ,;\n");
     }
-
-    // for(int i=0;i<token_vector.size();i++){
-    //     cout<<token_vector[i]<<endl;
-    // }
     cout<<endl;
 }
 
 void get_query(){
     char *query;
-    query = (char*) malloc (sizeof(char)*50);
+    query = (char*) malloc (sizeof(char)*1024);
     printf("enter query to test\n");
     fflush(stdin);
     fflush(stdout);
-    fgets(query,sizeof(char)*MAX_NAME,stdin);
-    fgets(query,sizeof(char)*MAX_NAME,stdin);
+    fgets(query, 1024, stdin);
+    fgets(query, 1024, stdin);
 
-    //
     char buffer[1024];
     strcpy(buffer, query);
     
-    // FIX: Added \n here to ensure the first command word is caught cleanly
     char *token = strtok(buffer, " \n");
     if(token){
         string token_temp(token);
-        if(token_temp != " " && token_temp != "\n"){
-            //cout<<"token:: "<<token<<endl;
-            if(token_temp == "select"){
-                tokenize_select(query);
-            }else if(token_temp == "create"){
-                tokenize_create(query);
-            }
+        if(token_temp == "select"){
+            tokenize_select(query);
+        }else if(token_temp == "create"){
+            tokenize_create(query);
         }
     }
-
-
-    //printf("\nquery:: %s\n",query);
+    free(query);
 }
 
 void parse_create(){
@@ -114,5 +92,4 @@ void parse_create(){
   string tbetween = s.substr(openpos+1, s.length()-openpos-2);
   cout<<token<<endl;
   cout<<tbetween<<endl;
-
 }
